39ea2fb2aa31925244db71c9404540ec42c97f34
Jaderson <jaderson6100@gmail.com>
2025-05-12 20:57:16 -0400

    Adicionado pagina de login do breeze

--- php/database/seeders/DatabaseSeeder.php
+++ php/database/seeders/DatabaseSeeder.php
@@ -19,5 +19,12 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        User::create([
+        'name' => 'Admin',
+        'email' => 'admin@email.com',
+        'password' => bcrypt('senha123')
+]);
+
     }
 }
