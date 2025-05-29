c32008b08af4c3cf13c539fd0110125e34baa526
Jaderson <jaderson6100@gmail.com>
2025-05-29 10:39:42 -0400

    adicionando titulo a tela de login

--- php/resources/views/auth/login.blade.php
+++ php/resources/views/auth/login.blade.php
@@ -1,6 +1,8 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
+    <h1 class="text-3xl font-bold text-green-custom text-center mb-2">LOGIN</h1>
+            <p class="text-center text-gray-700 mb-6">Entre com seus dados institucionais</p>
 
     <form method="POST" action="{{ route('login') }}">
         @csrf
@@ -24,13 +26,13 @@
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Remember Me -->
+        <!-- Remember Me/comentado pois nao esta no layout
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
                 <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
             </label>
-        </div>
+        </div> -->
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
