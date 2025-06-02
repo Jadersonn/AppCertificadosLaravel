cf94ab7433bf425b25c146bd5c1d89c5fbb2b9b1
Jaderson <jaderson6100@gmail.com>
2025-06-02 19:45:01 -0400

    corrigindo pagina de login para conseguir logar

--- php/app/Http/Requests/Auth/LoginRequest.php
+++ php/app/Http/Requests/Auth/LoginRequest.php
@@ -35,7 +35,7 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'numIdentidade' => ['required', 'string'],
+            'email' => ['required', 'string', 'email'],
             'password' => ['required', 'string'],
         ];
     }
@@ -49,11 +49,11 @@ class LoginRequest extends FormRequest
     {
         $this->ensureIsNotRateLimited();
 
-        if (! Auth::attempt($this->only('numIdentidade', 'password'), $this->boolean('remember'))) {
+        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
-                'numIdentidade' => trans('auth.failed'),
+                'email' => trans('auth.failed'),
             ]);
         }
 
