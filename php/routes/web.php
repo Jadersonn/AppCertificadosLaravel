c144e5bfa903aaa6fdc93c966e52cccf6e455d4c
jaderson <jaderson6100@gmail.com>
2025-06-05 11:07:32 -0400

    modificando rotas, apontando para cada classe de usuarios

--- php/routes/web.php
+++ php/routes/web.php
@@ -8,29 +8,35 @@ use App\Http\Controllers\AlunoController;
 Route::get('/', function () {
     return view('auth.login');
 });
-Route::get('/home', function () {
-    return view('auth.home');
-});
+
 Route::get('/resetSenha', function () {
     return view('resetSenha');
 });
 
-Route::get('/dashboard', function () {
+/*Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');*/
+
+//rotas para aluno autenticado
+Route::get('/aluno', function () {
+    $user = Auth::user();
+    return redirect()->route('aluno.show', ['id' => $user->numIdentidade]);
+})->middleware('auth', 'verified')->name('aluno');
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/aluno/{numIdentidade}', [AlunoController::class, 'show'])->name('aluno.show');
+    Route::get('/aluno/{numIdentidade}/delete', [AlunoController::class, 'destroy'])->name('aluno.delete');
+    Route::get('/aluno/create', [AlunoController::class, 'create'])->name('aluno.create');
+    Route::get('/aluno/{idnumIdentidade}/edit', [AlunoController::class, 'update'])->name('aluno.edit');
+});
+
 
 //rotas para usuario autenticado
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    //{rotas de separacao} aponta o usuario para a rota correta com base na sua funcao
-    
-    Route::get('/aluno', function () {
-        $user = Auth::user();
-        return redirect()->route('aluno.show', ['id' => $user->id]);
-    })->middleware('auth')->name('aluno');
 
     Route::get('/professor', function () {
         return view('professor');
@@ -39,12 +45,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/admin', function () {
         return view('admin');
     })->name('admin');
-
-    //rotas para aluno
-    Route::get('/aluno/{id}', [AlunoController::class, 'show'])->name('aluno.show');
-    Route::get('/aluno/{id}/delete', [AlunoController::class, 'destroy'])->name('aluno.delete');
-    Route::get('/aluno/create', [AlunoController::class, 'create'])->name('aluno.create');
-    Route::get('/aluno/{id}/edit', [AlunoController::class, 'update'])->name('aluno.edit');
 
 
     /*rotas para professor
@@ -60,7 +60,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/admin/create', [AdminController::class, 'create'])->name('admin.create');
     Route::get('/admin/{id}/edit', [AdminController::class, 'edit'])->name('admin.edit');
     Route::post('/admin/{id}/update', [AdminController::class, 'update'])->name('admin.update');*/
-    
+
 });
 
 require __DIR__ . '/auth.php';
