7f392ba558b6d29d67dd392b1143513bb62c3de2
lara <lara.riomayor@estudante.ifms.edu.br>
2025-07-21 10:18:49 -0300

    tela de prof atualizada

--- php/resources/views/professor/alunosAprovados.blade.php
+++ php/resources/views/professor/alunosAprovados.blade.php
@@ -1,25 +1,26 @@
 {{-- resources/views/professor/alunosAprovados.blade.php --}}
 <div class="professor-card">
-            <div class="professor-card-title">ALUNOS APROVADOS</div>
-            <table class="professor-card-table">
-                <thead>
+    <div class="professor-card-title">ALUNOS APROVADOS</div>
+    <div class="professor-card-table-scroll">
+        <table class="professor-card-table">
+            <thead>
+                <tr>
+                    <th>ALUNO</th>
+                    <th>TURMA</th>
+                    <th>CONCLUSÃO</th>
+                    <th>SUAP</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($aprovados as $item)
                     <tr>
-                        <th>ALUNO</th>
-                        <th>TURMA</th>
-                        <th>CONCLUSÃO</th>
-                        <th>SUAP</th>
+                        <td>{{ $item->name }}</td>
+                        <td>{{ $item->turma }}</td>
+                        <td>{{ $item->dataConclusao }}</td>
+                        <td></td>
                     </tr>
-                </thead>
-                <tbody>
-                    @foreach ($aprovados as $item)
-                        <tr>
-                            <td>{{ $item->name }}</td>
-                            <td>{{ $item->turma }}</td>
-                            <td>{{ $item->dataConclusao }}</td>
-                            <td></td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+                @endforeach
+            </tbody>
+        </table>
     </div>
+</div>
--- php/resources/views/professor/certificadosRecebidos.blade.php
+++ php/resources/views/professor/certificadosRecebidos.blade.php
@@ -4,75 +4,77 @@
 @endpush
 
 <div class="certificados-recebidos-box">
-    <h1>CERTIFICADOS RECEBIDOS</h1>
+    <h2>CERTIFICADOS RECEBIDOS</h2>
     <div class="certificados-tabela-container">
-        <table class="certificados-tabela">
-            <thead>
-                <tr>
-                    <th>ALUNO</th>
-                    <th>TURMA</th>
-                    <th>CATEGORIA</th>
-                    <th>ENVIO</th>
-                    <th>STATUS</th>
-                    <th>SEMESTRE</th>
-                    <th>HORAS</th>
-                    <th>ARQUIVO</th>
-                    <th>APROVAÇÃO</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($certificados as $certificado)
+        <div class="certificados-tabela-scroll">
+            <table class="certificados-tabela">
+                <thead>
                     <tr>
-                        <td>{{ $certificado->name }}</td>
-                        <td>{{ $certificado->turma }}</td>
-                        <td>{{ $certificado->idAtividadeComplementar }}</td>
-                        <td>{{ $certificado->dataEnvio }}</td>
-                        <td>{{ $certificado->statusCertificado ?? '-' }}</td>
-                        <td>{{ $certificado->semestre }}</td>
-                        <td>{{ $certificado->cargaHoraria }}</td>
-                        <td>
-                            <a href="{{ url('/certificados/visualizar/' . $certificado->idCertificado) }}"
-                                target="_blank">
-                                <img src="{{ asset('imagens/professor/download.ico') }}" alt="icone de download"
-                                    class="download-icon" width="25">
-                            </a>
-                        </td>
-
-                        <td>
-                            <!-- Rejeitar -->
-                            <form action="{{ route('certificados.rejeitar', $certificado->idCertificado) }}"
-                                method="POST" style="display:inline;">
-                                @csrf
-                                <button type="submit" style="border: none; background: none; cursor: pointer;">
-                                    <img src="{{ asset('imagens/professor/reject.ico') }}" alt="Rejeitar"
-                                        width="20">
-                                </button>
-                            </form>
-
-                            <!-- Editar -->
-                            <a href="#" class="btn-editar"
-                                data-url="{{ route('certificados.editar', $certificado->idCertificado) }}"
-                                style="text-decoration: none; color: inherit; display: inline-block;">
-                                <img src="{{ asset('imagens/professor/edit.ico') }}" alt="Editar" width="20">
-                            </a>
-
-
-
-
-                            <!-- Aprovar -->
-                            <form action="{{ route('certificados.aprovar', $certificado->idCertificado) }}"
-                                method="POST" style="display:inline;">
-                                @csrf
-                                <button type="submit" style="border: none; background: none; cursor: pointer;">
-                                    <img src="{{ asset('imagens/professor/like.ico') }}" alt="Aprovar" width="20">
-                                </button>
-                            </form>
-
-                        </td>
+                        <th>ALUNO</th>
+                        <th>TURMA</th>
+                        <th>CATEGORIA</th>
+                        <th>ENVIO</th>
+                        <th>STATUS</th>
+                        <th>SEMESTRE</th>
+                        <th>HORAS</th>
+                        <th>ARQUIVO</th>
+                        <th>APROVAÇÃO</th>
                     </tr>
-                @endforeach
-            </tbody>
+                </thead>
+                <tbody>
+                    @foreach ($certificados as $certificado)
+                        <tr>
+                            <td>{{ $certificado->name }}</td>
+                            <td>{{ $certificado->turma }}</td>
+                            <td>{{ $certificado->idAtividadeComplementar }}</td>
+                            <td>{{ $certificado->dataEnvio }}</td>
+                            <td>{{ $certificado->statusCertificado ?? '-' }}</td>
+                            <td>{{ $certificado->semestre }}</td>
+                            <td>{{ $certificado->cargaHoraria }}</td>
+                            <td>
+                                <a href="{{ url('/certificados/visualizar/' . $certificado->idCertificado) }}"
+                                    target="_blank">
+                                    <img src="{{ asset('imagens/professor/download.ico') }}" alt="icone de download"
+                                        class="download-icon" width="25">
+                                </a>
+                            </td>
 
-        </table>
+                            <td>
+                                <!-- Rejeitar -->
+                                <form action="{{ route('certificados.rejeitar', $certificado->idCertificado) }}"
+                                    method="POST" style="display:inline;">
+                                    @csrf
+                                    <button type="submit" style="border: none; background: none; cursor: pointer;">
+                                        <img src="{{ asset('imagens/professor/reject.ico') }}" alt="Rejeitar"
+                                            width="20">
+                                    </button>
+                                </form>
+
+                                <!-- Editar -->
+                                <a href="#" class="btn-editar"
+                                    data-url="{{ route('certificados.editar', $certificado->idCertificado) }}"
+                                    style="text-decoration: none; color: inherit; display: inline-block;">
+                                    <img src="{{ asset('imagens/professor/edit.ico') }}" alt="Editar" width="20">
+                                </a>
+
+
+
+
+                                <!-- Aprovar -->
+                                <form action="{{ route('certificados.aprovar', $certificado->idCertificado) }}"
+                                    method="POST" style="display:inline;">
+                                    @csrf
+                                    <button type="submit" style="border: none; background: none; cursor: pointer;">
+                                        <img src="{{ asset('imagens/professor/like.ico') }}" alt="Aprovar" width="20">
+                                    </button>
+                                </form>
+
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+
+            </table>
+        </div>
     </div>
 </div>
